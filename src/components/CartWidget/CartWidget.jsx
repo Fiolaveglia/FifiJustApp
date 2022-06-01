8d062f090353d1f0739008b21939dce83e9a4246
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-06-01 10:56:54 -0300

    Desinstalacion de iconos

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,11 @@
 import './CartWidget.css'
-import MaterialIcon from 'react-google-material-icons'
+// import MaterialIcon from 'react-google-material-icons'
 
 const CartWidget = (props) => {
     return (
         <div className='CartWidget'>
-            <MaterialIcon icon="shopping_bag" size={32} />
+            <img src='./cart-shopping-solid.svg' className='Cart' alt="cart"></img>
+            {/* <MaterialIcon icon="shopping_bag" size={32} /> */}
             <span className='Quantity'>{props.cantidad}</span>
         </div>
     )
