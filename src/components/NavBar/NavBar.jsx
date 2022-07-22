4b1bec6eadefab0c00d14d0228055d90ec6a30cf
Fiorella Laveglia <fiolaveglia@gmail.com>
2022-07-21 21:08:48 -0300

    Navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
-import {NavLink} from 'react-router-dom'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 import { useFirestore } from '../../hooks/useFirestore'
 import { obtenerCategorias } from '../../services/Firebase/firebase'
@@ -18,15 +19,47 @@ const NavBar = () => {
     }
     
     return (
-        <nav className="navbar container">
-                <NavLink to = "/"><img className="Logo " src= "../img/logo.png" alt="logo"></img></NavLink>
-                {data && data.map(cat => 
-                        <NavLink key={cat.id} to ={`/category/${cat.id}`} className='ButtonNav'>{cat.descripcion}</NavLink> 
-                    )}  
-                <NavLink to = "/cart"><CartWidget /></NavLink> 
-        </nav>
-    )
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        className="Logo col-"
+                        src="../img/logo.png"
+                        alt="logo"
+                    ></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {data &&
+                            data.map((cat) => (
+                                <NavLink
+                                    key={cat.id}
+                                    to={`/category/${cat.id}`}
+                                    className="ButtonNav col"
+                                >
+                                    {cat.descripcion}
+                                </NavLink>
+                            ))}
+                        <NavLink to="/cart" className='ButtonNav'>
+                            <CartWidget />
+                        </NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        // <nav className="navbar container">
+        //     <div className="row">
+        //         <NavLink to = "/"><img className="Logo col-" src= "../img/logo.png" alt="logo"></img></NavLink>
+        //         {data && data.map(cat =>
+        //                 <NavLink key={cat.id} to ={`/category/${cat.id}`} className='ButtonNav col'>{cat.descripcion}</NavLink>
+        //             )}
+        //         <NavLink to = "/cart"><CartWidget /></NavLink>
+        //     </div>
+        // </nav>
+    );
 }
 
 
 export default NavBar 
+
